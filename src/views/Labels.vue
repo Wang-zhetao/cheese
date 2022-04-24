c72799cfa3a2aff8bd3b70696cb5cdddcbb91f11
Wang-zhetao <wzt15857662906@163.com>
2022-04-24 17:35:13 +0800

    创建 <Layout/>

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,25 +1,9 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      标签页面
-    </div>
-    <Nav/>
-  </div>
+  <Layout>
+    Labels.vue
+  </Layout>
 </template>
 
 <script lang="ts">
 </script>
 
-<style lang="scss" scoped>
-.nav-wrapper{
-  border: 1px solid green;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-.content{
-  border: 1px solid blue;
-  flex-grow: 1;
-  overflow: auto;
-}
-</style>
