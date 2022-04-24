f3c313635f65ace57e741f6336873c0cdf7e3bcb
Wang-zhetao <wzt15857662906@163.com>
2022-04-24 20:59:15 +0800

    收集四个组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,27 +1,44 @@
 <template>
   <Layout class-prefix="layout">
-    <NumberPad/>
-    <Types/>
-    <Notes/>
-    <Tags :data-source.sync="tags"/>
+    {{record}}
+    <NumberPad :value.sync="record.amount"/>
+    <Types :value.sync="record.type"/>
+    <Notes @update:value="onUpdateNotes"/>
+    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
 
-<script>
+<script lang="ts">
+import Vue from "vue";
 import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
-export default {
-  name: "Money",
-  components: {Tags, Notes, Types, NumberPad},
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  data(){
-    return{
-      tags:['衣', '食', '住', '行','避孕套']
-    }
-  }
+import {Component} from "vue-property-decorator";
 
+type Record = {
+  tags: string[]
+  notes: string
+  type: string
+  amount: number
+}
+// eslint-disable-next-line no-undef
+@Component({
+  components: {Tags, Notes, Types, NumberPad}
+})
+export default class Money extends Vue {
+  tags = ['衣', '食', '住', '行', '彩票'];
+  record: Record = {
+    tags: [], notes: '', type: '-', amount: 0
+  };
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  onUpdateTags(value: string[]) {
+    this.record.tags = value;
+  }
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  onUpdateNotes(value: string) {
+    this.record.notes = value;
+  }
 }
 </script>
 
